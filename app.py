6e12f5fa78adfc42db30695eea157240dccf8d10
Junwon Kwon <snsxhdz@naver.com>
2022-11-25 14:58:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
+import os
+import socket
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello Flask Test'
+    ip = socket.gethostbyname(socket.gethostname())
+    zone = os.environ.get('ZONE', '')
+    return f'[{zone}] hello! {ip}'
 
 if __name__ == '__main__':
     app.run()
