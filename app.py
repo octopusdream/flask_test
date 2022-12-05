02b42fcb127716de2d117fd736ae11ed5544669b
SeungHun Hyun <gozldshid@naver.com>
2022-12-05 21:22:22 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,14 @@
 import subprocess
 import socket
-from flask import Flask, Blueprint
+from flask import Flask, request
 from prometheus_client import make_wsgi_app
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
 from werkzeug.serving import run_simple
-from flask_prometheus_metrics import register_metrics
+from flask_prometheus_metrics import register_metrics, PrometheusMetrics
 
-CONFIG = {"version": "v0.1.2", "config": "staging"}
-MAIN = Blueprint("main", __name__)
 app = Flask(__name__)
+metrics = PrometheusMetrics(app)
 
-
-def register_blueprints(app):
-    """
-    Register blueprints to the app
-    """
-    app.register_blueprint(MAIN)
 
 @app.route('/')
 def hello():
@@ -24,33 +17,8 @@ def hello():
     zone = subprocess.check_output(cmd, shell=True)
     return f'[{zone}] "hello! {app}" from {ip}'
 
-def create_app(config):
-    """
-    Application factory
-    """
-    app = Flask(__name__)
-
-    register_blueprints(app)
-    register_metrics(app, app_version=config["version"], app_config=config["config"])
-    return app
-
-
-def create_dispatcher() -> DispatcherMiddleware:
-    """
-    App factory for dispatcher middleware managing multiple WSGI apps
-    """
-    main_app = create_app(config=CONFIG)
-    return DispatcherMiddleware(main_app.wsgi_app, {"/metrics": make_wsgi_app()})
-
 
 if __name__ == "__main__":
-    run_simple(
-        "0.0.0.0",
-        5002,
-        create_dispatcher(),
-        use_reloader=True,
-        use_debugger=True,
-        use_evalex=True,
-    )
-#     app.run()
+    
+    app.run("0.0.0.0", 5000)
     
