0a54038f13c1381a07c12caed153009f12387f38
SeungHun Hyun <gozldshid@naver.com>
2022-12-05 15:10:23 +0900

    Update app.py

--- app.py
+++ app.py
@@ -24,5 +24,5 @@ if __name__ == '__main__':
     # Plug metrics WSGI app to your main app with dispatcher
     dispatcher = DispatcherMiddleware(app.wsgi_app, {"/metrics": make_wsgi_app()})
 
-    run_simple(hostname="localhost", port=5000, application=dispatcher)
+    run_simple(hostname="localhost", port=5001, application=dispatcher)
     app.run()
